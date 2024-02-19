127268a2f3d017bcddfe6dc9629a97faa5d2924f
coryTu <202164050406@mail.scut.edu.cn>
2024-02-19 22:30:18 +0100

    update configuration file

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SmartDoorBell"
+include(":app")
+ 
