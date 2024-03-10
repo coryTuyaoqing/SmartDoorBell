5ca48731513978c2bb1a09b8050e90af44475d1a
Matt Delalieux <matt.delalieux@icloud.com>
2024-03-10 14:59:52 +0100

    Working registration, simply have to add passwords to database

--- app/src/main/java/com/example/smartdoorbell/MainActivity.java
+++ app/src/main/java/com/example/smartdoorbell/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.smartdoorbell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btnProfile = findViewById(R.id.btnProfile);
+
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,4 +41,8 @@ public class MainActivity extends AppCompatActivity {
     public void btnOnClick(View view){
 
     }
+
+
+
 }
+
--- app/src/main/java/com/example/smartdoorbell/Profile.java
+++ app/src/main/java/com/example/smartdoorbell/Profile.java
@@ -1,5 +1,6 @@
 package com.example.smartdoorbell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,25 +9,78 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Profile extends AppCompatActivity {
+import java.io.IOException;
 
-    private TextView txtFirstName, txtSecondName, txtEmail;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Profile extends AppCompatActivity {
+    OkHttpClient client;
+    private TextView txtFirstName, txtSecondName, txtEmail, registrationStatus;
     Button btnRegister;
-    EditText edtTxtFirstName, edtTxtSecondName, edtTxtEmail;
+    EditText edtTxtFirstName, edtTxtSecondName, edtTxtEmail, edtPassWord, edtPassWordAgain;
+
+    String postURL = "https://studev.groept.be/api/a23ib2a04/Registration_POST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        client = new OkHttpClient();
         initView();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtFirstName.setText(edtTxtFirstName.getText().toString());
-                txtSecondName.setText(edtTxtSecondName.getText().toString());
-                txtEmail.setText(edtTxtEmail.getText().toString());
+                String fName = edtTxtFirstName.getText().toString();
+                String lName = edtTxtSecondName.getText().toString();
+                String email = edtTxtEmail.getText().toString();
+                String password = edtPassWord.getText().toString();
+                String passwordagain = edtPassWordAgain.getText().toString();
+                String errorMsg = "Passwords do not match";
+
+                if (!fName.isEmpty() && !lName.isEmpty() && !email.isEmpty() && !password.isEmpty() && !passwordagain.isEmpty() && passwordagain.equals(password)) {
+                    // Update TextViews with the entered data
+                    txtFirstName.setText(fName);
+                    txtSecondName.setText(lName);
+                    txtEmail.setText(email);
+
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("fn", fName)
+                            .add("ln", lName)
+                            .add("mail", email)
+                            .build();
+
+                    Request request = new Request.Builder()
+                            .url(postURL)
+                            .post(requestBody)
+                            .build();
+
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                            e.printStackTrace();
+                            System.out.println(postURL);
+                        }
+
+                        @Override
+                        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                            if (!response.isSuccessful()) {
+                                // Handle unsuccessful response
+                                // You may want to show an error message to the user
+                            }
+                        }
+                    });
+                } else {
+                    if (!passwordagain.equals(password)) {
+                        registrationStatus.setText(errorMsg);
+                    }
+                }
             }
         });
     }
@@ -39,7 +93,8 @@ public class Profile extends AppCompatActivity {
         edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
         edtTxtSecondName = findViewById(R.id.edtTxtSecondName);
         edtTxtEmail = findViewById(R.id.edtTxtEmail);
+        edtPassWord = findViewById(R.id.edtPassWord);
+        edtPassWordAgain = findViewById(R.id.edtPassWordAgain);
+        registrationStatus = findViewById(R.id.registrationStatus); // Initialize registrationStatus TextView
     }
-
-
 }
