250567ad44dfe1d78a71c7397d1dae416ace59d9
coryTu <202164050406@mail.scut.edu.cn>
2024-03-11 11:01:04 +0100

    add new profile page

--- app/src/main/java/com/example/smartdoorbell/ProfileActivity.java
+++ app/src/main/java/com/example/smartdoorbell/ProfileActivity.java
@@ -4,102 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class ProfileActivity extends AppCompatActivity {
-    OkHttpClient client;
-    private TextView txtFirstName, txtSecondName, txtEmail, registrationStatus;
-    Button btnRegister;
-    EditText edtTxtFirstName, edtTxtSecondName, edtTxtEmail, edtPassWord, edtPassWordAgain;
 
-    String postURL = "https://studev.groept.be/api/a23ib2a04/Registration_POST";
+    Button btnProfileHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        client = new OkHttpClient();
-//        initView();
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnProfileHome = findViewById(R.id.btnProfileHome);
+
+        btnProfileHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fName = edtTxtFirstName.getText().toString();
-                String lName = edtTxtSecondName.getText().toString();
-                String email = edtTxtEmail.getText().toString();
-                String password = edtPassWord.getText().toString();
-                String passwordagain = edtPassWordAgain.getText().toString();
-                String errorMsg = "Passwords do not match";
-
-                if (!fName.isEmpty() && !lName.isEmpty() && !email.isEmpty() && !password.isEmpty() && !passwordagain.isEmpty() && passwordagain.equals(password)) {
-                    // Update TextViews with the entered data
-                    txtFirstName.setText(fName);
-                    txtSecondName.setText(lName);
-                    txtEmail.setText(email);
-
-                    RequestBody requestBody = new FormBody.Builder()
-                            .add("fn", fName)
-                            .add("ln", lName)
-                            .add("mail", email)
-                            .add("pw",password)
-                            .build();
-
-                    Request request = new Request.Builder()
-                            .url(postURL)
-                            .post(requestBody)
-                            .build();
-
-                    Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
-                    startActivity(intent);
-
-                    client.newCall(request).enqueue(new Callback() {
-                        @Override
-                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                            e.printStackTrace();
-                            System.out.println(postURL);
-                        }
-
-                        @Override
-                        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                            if (!response.isSuccessful()) {
-                                // Handle unsuccessful response
-                                // You may want to show an error message to the user
-                            }
-                        }
-                    });
-                } else {
-                    if (!passwordagain.equals(password)) {
-                        registrationStatus.setText(errorMsg);
-                    }
-                }
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    private void initView() {
-        txtFirstName = findViewById(R.id.txtFirstName);
-        txtSecondName = findViewById(R.id.txtSecondName);
-        txtEmail = findViewById(R.id.txtEmail);
-        btnRegister = findViewById(R.id.btnRegist);
-        edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
-        edtTxtSecondName = findViewById(R.id.edtTxtSecondName);
-        edtTxtEmail = findViewById(R.id.edtTxtEmail);
-        edtPassWord = findViewById(R.id.edtPassWord);
-        edtPassWordAgain = findViewById(R.id.edtPassWordAgain);
-        registrationStatus = findViewById(R.id.registrationStatus); // Initialize registrationStatus TextView
+//        Context context = null;
+//        File.createTempFile(filename, null, context.getCacheDir());
+//        File file = new File(context.getFilesDir(), filename);
     }
 }
