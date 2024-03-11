3b907fc4085aff196ed76011d3c1f45cb0dfbaef
Matt Delalieux <matt.delalieux@icloud.com>
2024-03-11 09:29:24 +0100

    Working registration, simply have to add passwords to database

--- app/src/main/java/com/example/smartdoorbell/Profile.java
+++ app/src/main/java/com/example/smartdoorbell/Profile.java
@@ -3,6 +3,7 @@ package com.example.smartdoorbell;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -54,12 +55,16 @@ public class Profile extends AppCompatActivity {
                             .add("fn", fName)
                             .add("ln", lName)
                             .add("mail", email)
+                            .add("pw",password)
                             .build();
 
                     Request request = new Request.Builder()
                             .url(postURL)
                             .post(requestBody)
                             .build();
+
+                    Intent intent = new Intent(Profile.this, MainActivity.class);
+                    startActivity(intent);
 
                     client.newCall(request).enqueue(new Callback() {
                         @Override
