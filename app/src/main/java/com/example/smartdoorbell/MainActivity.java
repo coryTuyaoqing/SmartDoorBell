dc4ed339c9c8904c35861b1a137f2be46611c645
coryTu <202164050406@mail.scut.edu.cn>
2024-03-18 09:05:22 +0100

    Image button, get access to the raspberry pi http server

--- app/src/main/java/com/example/smartdoorbell/MainActivity.java
+++ app/src/main/java/com/example/smartdoorbell/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.smartdoorbell;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnProfile;
     private TextView txtDoorStatus;
+    private ImageView cameraPhoto;
 
     private ImageButton imgBtnLock, imgBtnSpeak, imgBtnCamera;
     private boolean doorClosed, speakable;
+    private OkHttpClient client;
+    final private String url = "http://192.168.1.103/photo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +85,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        imgBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request request = new Request.Builder().url(url).build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        e.printStackTrace();
+                        System.out.println(url);
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if (response.isSuccessful()){
+                            final Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                            // Remember to set the bitmap in the main thread.
+                            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    cameraPhoto.setImageBitmap(bitmap);
+                                }
+                            });
+                        }else {
+                            //Handle the error
+                        }
+                    }
+                });
+            }
+        });
     }
 
     private void intiView() {
@@ -76,8 +123,10 @@ public class MainActivity extends AppCompatActivity {
         imgBtnLock = findViewById(R.id.imgBtnLock);
         imgBtnSpeak = findViewById(R.id.imgBtnSpeak);
         imgBtnCamera = findViewById(R.id.imgBtnTakePhoto);
+        cameraPhoto = findViewById(R.id.cameraPhoto);
         doorClosed = true;
         speakable = false;
+        client = new OkHttpClient();
     }
 
 }
